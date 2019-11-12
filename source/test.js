0d1b43cc12c83f9ddb8d49a1acaf8718a026f863
Benjamin Lupton <b@lupton.cc>
2019-11-13 07:51:09 +0800

    v3.3.0 - boundation, changelog, version bump

--- source/test.js
+++ source/test.js
@@ -1,30 +1,26 @@
 /* eslint no-console:0 */
+'use strict'
 
 // Import
-const {equal, deepEqual} = require('assert-helpers')
-const joe = require('joe')
+const { equal, deepEqual } = require('assert-helpers')
+const kava = require('kava')
 const semver = require('semver')
 const eachr = require('../')
 
 // Test
-joe.suite('eachr', function (suite, test) {
-
+kava.suite('eachr', function(suite, test) {
 	// Arrays
-	test('should cycle arrays', function () {
+	test('should cycle arrays', function() {
 		// Prepare
 		const arr = ['first', 'second', 'third']
-		const expected = [
-			['first',  0, arr, arr],
-			['second', 1, arr, arr],
-			'break'
-		]
+		const expected = [['first', 0, arr, arr], ['second', 1, arr, arr], 'break']
 		const actual = []
 
 		// Cycle
-		const result = eachr(arr, function (value, key, subject) {
+		const result = eachr(arr, function(value, key, subject) {
 			equal(subject, arr, 'subject is as expected')
 			actual.push([value, key, subject, this])
-			if ( value === 'second' ) {
+			if (value === 'second') {
 				actual.push('break')
 				return false
 			}
@@ -36,21 +32,21 @@ joe.suite('eachr', function (suite, test) {
 	})
 
 	// Objects
-	test('should cycle objects', function () {
+	test('should cycle objects', function() {
 		// Prepare
-		const obj = {a: 'first', b: 'second', c: 'third'}
+		const obj = { a: 'first', b: 'second', c: 'third' }
 		const expected = [
-			['first',  'a', obj, obj],
+			['first', 'a', obj, obj],
 			['second', 'b', obj, obj],
 			'break'
 		]
 		const actual = []
 
 		// Cycle
-		const result = eachr(obj, function (value, key, subject) {
+		const result = eachr(obj, function(value, key, subject) {
 			equal(subject, obj, 'subject is as expected')
 			actual.push([value, key, subject, this])
-			if ( value === 'second' ) {
+			if (value === 'second') {
 				actual.push('break')
 				return false
 			}
@@ -62,26 +58,32 @@ joe.suite('eachr', function (suite, test) {
 	})
 
 	// Objects
-	test('should cycle maps', function () {
-		if ( semver.satisfies(process.version, '>=0.12') === false ) {
-			console.warn(`This version of node ${process.version} does not support Maps, skipping test...`)
+	test('should cycle maps', function() {
+		if (semver.satisfies(process.version, '>=0.12') === false) {
+			console.warn(
+				`This version of node ${process.version} does not support Maps, skipping test...`
+			)
 			return
 		}
 
 		// Prepare
-		const map = new Map([['a', 'first'], ['b', 'second'], ['c', 'third']])
+		const map = new Map([
+			['a', 'first'],
+			['b', 'second'],
+			['c', 'third']
+		])
 		const expected = [
-			['first',  'a', map, map],
+			['first', 'a', map, map],
 			['second', 'b', map, map],
 			'break'
 		]
 		const actual = []
 
 		// Cycle
-		const result = eachr(map, function (value, key, subject) {
+		const result = eachr(map, function(value, key, subject) {
 			equal(subject, map, 'subject is as expected')
 			actual.push([value, key, subject, this])
-			if ( value === 'second' ) {
+			if (value === 'second') {
 				actual.push('break')
 				return false
 			}
